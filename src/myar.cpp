23fff9c37df83d9eb96f5c9a94ed0538f0a1e253
1n5aN1aC <joshuavillwo@gmail.com>
2012-10-16 20:48:12 -0700

    Transitioning Systems

---
+++ src/myar.cpp
@@ -0,0 +1,11 @@
+// myar.cpp : main project file.
+
+#include "stdinc.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine(L"Hello World");
+    return 0;
+}
