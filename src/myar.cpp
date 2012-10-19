a8c93759cc8491c00a0e8d0c279e41b0a6c7ac4e
1n5aN1aC <joshuavillwo@gmail.com>
2012-10-19 11:04:22 -0700

    More Stuff

--- src/myar.cpp
+++ src/myar.cpp
@@ -10,8 +10,43 @@ using namespace std;
 string archivePath;  //Path to the archive file we are opening / reading.
 int archiveFdRd = -2; //FileDescriptor for Reading.
 int archiveFdWr = -2; //FileDescriptor for writing.
-//ar_hdr* fileList = new ar_hdr[1];;
-ar_hdr* fileList[3];
+ar_hdr* fileList = new ar_hdr[5];
+
+//Method Declarations
+bool kill(string message);
+bool error(string message);
+bool checkValidAr(int fd);
+bool openArchiveRead(string path, bool check);
+bool openArchiveWrite(string path);
+bool closeArchive();
+void readHeaderAt(int start, int arrayLoc);
+ar_hdr* bruteForceHeaderAt(int start);
+bool getList();
+void returnShortList();
+void returnFullList();
+bool fileIsInArchive(string file);
+bool writeFromArchive(string file);
+bool writeFromArchiveLoc(int start, int length);
+bool addToArchiveEnd(string file);
+bool addDirToArchiveEnd(string file);
+
+int main(int argc, char** argv)
+{
+	openArchiveRead("./ex.a", true);
+	openArchiveWrite("./ex.a");
+
+	bruteForceHeaderAt(9);
+
+	//getList();
+
+	//cout << fileList[0].ar_name;
+
+    cout << "Program Finished Main :)" << endl;
+	
+
+	closeArchive();
+	return 0;
+}
 
 bool kill(string message) { //Ends program with error.
 	perror(message.c_str());
@@ -23,13 +58,9 @@ bool error(string message) {
 	return false;
 }
 
-int curLoc(int fd) {
-	return lseek(fd, SEEK_CUR, 0);
-}
-
 bool checkValidAr(int fd) {
 	char head[SARMAG];
-	lseek(fd, SEEK_SET, 0);
+	lseek(fd, 0, SEEK_SET);
 	read(fd, head, SARMAG);
 	if (strncmp(head, ARMAG, 8) == 0)
 		return true;
@@ -48,7 +79,7 @@ bool openArchiveRead(string path, bool check) {
 	else
 		return true;
 }
-bool openArchiveWrite(string path, bool check) {
+bool openArchiveWrite(string path) {
 	if (archivePath != path)
 		return kill("Please Open For Reading Before Writing.");
 	archiveFdWr = open(path.c_str(), O_WRONLY);
@@ -56,58 +87,39 @@ bool openArchiveWrite(string path, bool check) {
 		return kill("Can't open output file.");
 	else if(archiveFdWr == -2)
 		return kill("Something Really Bad Happened.");
-	else if (check == true)
-		return checkValidAr(archiveFdWr);
 	else {
 		return true;
 	}
 }
 bool closeArchive() {
 	if (close(archiveFdRd) == -1)
-		return error("could not close Read fd");
+		return kill("could not close Read fd");
 	if (close(archiveFdWr) == -1)
-		return error("could not close Write fd");
+		return kill("could not close Write fd");
+	cout << "DERP CLOSED FILE" << endl;
 	return true;
 }
 
-ar_hdr* readHeaderAt(int start) {
-	lseek(archiveFdRd, SEEK_SET, start);
+void readHeaderAt(int start, int arrayLoc) {
+	lseek(archiveFdRd, start, SEEK_SET);								//seek to where the header starts
+	struct ar_hdr* h = (struct ar_hdr*)malloc(sizeof(struct ar_hdr));	//Create a temporary struct.
+	read(archiveFdRd, h, sizeof(struct ar_hdr));						//Read into temporary struct
+
+	cout << fileList[0].ar_name << endl;
+
+	//memcpy ( &fileList[arrayLoc], &h, sizeof(h) );						//Save read struct into struct array
+}
+ar_hdr* bruteForceHeaderAt(int start) {
 	struct ar_hdr* h = (struct ar_hdr*)malloc(sizeof(struct ar_hdr));
-	read(archiveFdRd, h, sizeof(struct ar_hdr));
-	if (archiveFdRd == -1) {
-		perror("Failed to read archive!");
-		exit(-1); 
-	}
+
+	lseek(archiveFdRd, start, SEEK_SET);
+	read(archiveFdRd, h.ar_name, 16);
+	cout << h.ar_name;
+
 	return h;
 }
 bool getList() {
-	lseek(archiveFdRd, SEEK_SET, SARMAG);
-	struct ar_hdr* h = readHeaderAt(curLoc(archiveFdRd));
-
-	int i = sizeof(fileList); //find size of array.  since arrays start at 0, size will be end + 1
-	memcpy ( &fileList[i], &h, sizeof(h) );  //Save read struct into struct array
-	
+	readHeaderAt(8, 0);
+	//cout << fileList[0].ar_name << endl;
 	return true;
-}
-void returnShortList();
-void returnFullList();
-bool fileIsInArchive(string file);
-
-bool writeFromArchive(string file);
-bool writeFromArchiveLoc(int start, int length);
-
-bool addToArchiveEnd(string file);
-bool addDirToArchiveEnd(string file);
-
-int main(int argc, char** argv)
-{
-	openArchiveRead("./ex.a", true);
-	openArchiveWrite("./ex.a", true);
-	closeArchive();
-	getList();
-	cout << fileList[0]->ar_name;
-
-    cout << "Program Finished Main :)" << "\n";
-	
-	return 0;
 }
